b614827d2343842c99a803286ee6ad2653ba1393
victorhdsp <victorhugods.pereira@gmail.com>
2025-04-05 13:14:47 -0300

    change nuxt welcome to nuxt pages and import equipment data

--- app.vue
+++ app.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div id="body">
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+import { getEquipments } from './assets/js/getEquipments';
+import type { IEquipment } from './assets/types/equipament';
+import { useEquipmentStore } from './store/equipments';
+
+const useEquipment = useEquipmentStore();
+
+const equipments: IEquipment[] = getEquipments();
+useEquipment.addEquipments(equipments);
+
+</script>
