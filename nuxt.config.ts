0576f28ed5d2f0fbb325ac41a896bd137f949adf
victorhdsp <victorhugods.pereira@gmail.com>
2025-04-05 15:31:40 -0300

    desable dev tools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
+  pages: true,
   postcss: {
     plugins: {
       tailwindcss: {},
