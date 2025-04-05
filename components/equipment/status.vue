16364977e3f74a733e0a2b9c1be364b3ebcdf2df
victorhdsp <victorhugods.pereira@gmail.com>
2025-04-05 13:16:36 -0300

    status component

---
+++ components/equipment/status.vue
@@ -0,0 +1,38 @@
+<template>
+    <span class="equipament-status">
+        <div class="color" :style="`--color: ${props.color};`"/>
+        <p class="name">{{props.name}}</p>
+    </span>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps({
+    name: {
+        type: String,
+        required: true
+    },
+    color: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+    .equipament-status {
+        @apply flex items-center gap-2;
+        .color {
+            @apply rounded-full;
+            @apply w-2 h-2;
+            @apply bg-[var(--color)];
+        }
+        .name {
+            @apply text-sm;
+            @apply font-semibold;
+            @apply text-gray-800;
+            @apply m-1;
+        }
+    }
+</style>
