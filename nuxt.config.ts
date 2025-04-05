dda0bfb452f4c99e5285339cad9e8bcaa916fa1d
victorhdsp <victorhugods.pereira@gmail.com>
2025-04-05 13:15:56 -0300

    include pinia, typescript alias and tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,14 +2,28 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   modules: [
+    '@nuxtjs/leaflet',
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/ui',
     '@nuxt/scripts',
     '@nuxt/test-utils',
-    '@nuxt/image'
-  ]
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+  ],
+  
+  css: [ 'assets/css/main.scss' ],
+
+  pinia: {
+    storesDirs: ['./stores/**'],
+  },
 })
